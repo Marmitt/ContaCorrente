fa94036011b6545a31f4328d7d88437a8f22d8f4
Mateus Marmitt <lmateus.marmitt@gmail.com>
2017-08-29 19:57:50 -0300

    Teste 2

--- src/br/com/institutoivoti/test/ContaTesteTest.java
+++ src/br/com/institutoivoti/test/ContaTesteTest.java
@@ -6,7 +6,7 @@ import org.junit.Test;
 import br.com.institutoivoti.Conta;
 import junit.framework.TestCase;
 
-public class ContaTesteTest extends TestCase {
+public class TestaContaCorrente{
 	
 	private Conta conta;
 	
@@ -17,8 +17,78 @@ public class ContaTesteTest extends TestCase {
 	
 	@Test
 	public void testaNome() {
-		conta.Titular('flex');
-		assertEquals(conta.getTitular(), 'flex');
+		conta.Titular("flex");
+		assertEquals(conta.getTitular(), "flex");
+	}
+	
+	@Test
+	public void testaAgencia() {
+		conta.Agencia("Junior");
+		assertEquals(conta.getAgencia(), "Junior");
+	}
+	
+	@Test
+	public void testaNumeroConta() {
+		conta.NumeroConta("4625-4");
+		assertEquals(conta.getNumero(), "4625-4");
+	}
+	
+	@Test
+	public void testaSaldo() {
+		conta.Saldo(78596.30);
+		assertEquals(conta.getSaldo(), 78596.30, 0);
+	}
+	
+	@Test
+	public void testaSaldoZerado() {
+		conta.Saldo(0);
+		assertEquals(conta.getSaldo(), 0, 0);
+	}
+	
+	@Test
+	public void incrementaSaldo() {
+		conta.Saldo(0);
+		conta.IncrementaSaldo(20);
+		assertEquals(conta.incrementaSaldo(), 20, 0);
+	}
+	
+	@Test
+	public void debito() {
+		conta.Saldo(50);
+		conta.DebitaDaConta(20);
+		assertEquals(conta.getDebitoConta(), 30, 0);
+	}
+	
+	@Test
+	public void saldoNegativo() {
+		conta.Saldo(0);
+		conta.DebitaDaConta(10);
+		assertEquals(conta.getDebitoConta(), -10, 0);
+	}
+	
+	@Test
+	public void incrementaSaldoNegativo() {
+		conta.Saldo(-10);
+		conta.IncrementaSaldo(20);
+		assertEquals(conta.incrementaSaldo(), 10, 0);
+	}
+	
+	@Test
+	public void testaLimiteSaldo() {
+		conta.Saldo(100);
+		conta.DebitaDaConta(120);
+		conta.limiteConta(-30);
+		assertEquals(conta.getLimite(), -20, 0);
+	}
+	
+	@Test
+	public void excedendoLimiteConta() {
+		conta.Saldo(100);
+		conta.DebitaDaConta(110);
+		conta.limiteConta(-10);
+		assertEquals(conta.getLimite(), 2, 0);
 	}
 
 }
+
+
